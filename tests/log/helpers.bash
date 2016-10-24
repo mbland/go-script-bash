0a7bc5ec2cf7e1800791ecfd308d66bbe81b25bb
Mike Bland <mbland@acm.org>
2016-10-24 18:24:46 -0400

    lib/log: Add logging module
    This provides standard logging facilities for different levels of
    messages. Uses terminal formatting for log level prefixes when output is
    to a terminal, or `_GO_LOG_FORMATTING` is set.

---
+++ tests/log/helpers.bash
@@ -0,0 +1,103 @@
+#! /bin/bash
+#
+# Helper functions for `lib/log` tests.
+
+run_log_script() {
+  create_test_go_script ". \"\$_GO_USE_MODULES\" 'log'" "$@"
+  run "$TEST_GO_SCRIPT"
+}
+
+format_label() {
+  local label="$1"
+
+  if [[ -z "$__GO_LOG_INIT" ]]; then
+    . "$_GO_CORE_DIR/lib/log"
+    _GO_LOG_FORMATTING='true'
+    _@go.log_init
+  fi
+
+  local __go_log_level_index=0
+  if ! _@go.log_level_index "$label"; then
+    echo "Unknown log level label: $label" >&2
+    return 1
+  fi
+  echo "${__GO_LOG_LEVELS_FORMATTED[$__go_log_level_index]}"
+}
+
+__expected_log_line() {
+  local level="$1"
+  local message="$2"
+
+  if [[ "${level:0:3}" == '\e[' ]]; then
+    stripped_level="${level//\\e\[[0-9]m}"
+    stripped_level="${stripped_level//\\e\[[0-9][0-9]m}"
+    stripped_level="${stripped_level//\\e\[[0-9][0-9][0-9]m}"
+    level="${level}${padding:0:$((${#padding} - ${#stripped_level}))}"
+    echo -e "$level $message\e[0m"
+  else
+    level="${level}${padding:0:$((${#padding} - ${#level}))}"
+    echo "$level $message"
+  fi
+}
+
+__all_output_consumed() {
+  local index="$1"
+  local remaining_lines="$((${#lines[@]} - index))"
+
+  if [[ "$remaining_lines" -gt '0' ]]; then
+    if [[ "$remaining_lines" -eq '1' ]]; then
+      echo "There is one more line of output than expected:" >&2
+    else
+      echo "There are $remaining_lines more lines of output than expected:" >&2
+    fi
+    local IFS=$'\n'
+    echo "${lines[*]:$index}" >&2
+    return 1
+
+  elif [[ "$remaining_lines" -lt '0' ]]; then
+    remaining_lines="$((-remaining_lines))"
+    if [[ "$remaining_lines" -eq '1' ]]; then
+      echo "There is one fewer line of output than expected." >&2
+    else
+      echo "There are $remaining_lines fewer lines of output than expected." >&2
+    fi
+    return 1
+  fi
+}
+
+assert_log_equals() {
+  local level
+  local padding=''
+  local expected_line
+  local num_errors=0
+  local remaining_lines=0
+  local i
+
+  . "$_GO_CORE_DIR/lib/log"
+  for level in "${_GO_LOG_LEVELS[@]}"; do
+    while [[ "${#padding}" -lt "${#level}" ]]; do
+      padding+=' '
+    done
+  done
+
+  for ((i=0; $# != 0; ++i)); do
+    expected_line="$(__expected_log_line "$1" "$2")"
+    if ! assert_equal "${lines[$i]}" "$expected_line" "line $i"; then
+      ((++num_errors))
+    fi
+    set +o functrace
+
+    if ! shift 2; then
+      echo "ERROR: Wrong number of arguments for log line $i." >&2
+      return 1
+    fi
+  done
+
+  if ! __all_output_consumed "$i"; then
+    ((++num_errors))
+  fi
+
+  if [[ "$num_errors" -ne '0' ]]; then
+    return_from_bats_assertion "$BASH_SOURCE" 1
+  fi
+}
