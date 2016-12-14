842433805b1d8b7ee8a8ac50b3f9ccf7fb775d9e
Mike Bland <mbland@acm.org>
2016-12-14 12:10:09 -0500

    print_stack_trace: Make skip_callers arg numerical
    Realized it'd be very handy to support the removal of callers beyond the
    immediate caller in the `lib/log` module after experimenting with
    `@go.print_stack_trace` to `@go.log FATAL` calls.

--- go-core.bash
+++ go-core.bash
@@ -141,16 +141,36 @@ declare _GO_SEARCH_PATHS=("$_GO_CORE_DIR/libexec")
 
 # Prints the stack trace at the point of the call.
 #
+# If supplied, the `skip_callers` argument should be a positive integer (i.e. 1
+# or greater) to remove the caller (and possibly the caller's caller, and so on)
+# from the resulting stack trace.
+#
 # Arguments:
-#   omit_caller: If set, this function's caller is removed from the output
+#   skip_callers: The number of callers to skip over when printing the stack
 @go.print_stack_trace() {
-  local start_index="${1:+2}"
+  local skip_callers="$1"
+  local result=0
   local i
 
-  for ((i=${start_index:-1}; i != ${#FUNCNAME[@]}; ++i)); do
+  if [[ -n "$skip_callers" && ! "$skip_callers" =~ ^[1-9][0-9]*$ ]]; then
+    @go.printf '%s argument %s not a positive integer; printing full stack\n' \
+      "$FUNCNAME" "'$skip_callers'" >&2
+    result=1
+  elif [[ "$skip_callers" -ge "${#FUNCNAME[@]}" ]]; then
+    @go.printf '%s argument %d exceeds stack size %d; printing full stack\n' \
+      "$FUNCNAME" "$skip_callers" "$((${#FUNCNAME[@]} - 1))" >&2
+    result=1
+  fi
+
+  if [[ "$result" -ne '0' ]]; then
+    skip_callers=0
+  fi
+
+  for ((i=$skip_callers + 1; i != ${#FUNCNAME[@]}; ++i)); do
     @go.printf '  %s:%s %s\n' "${BASH_SOURCE[$i]}" "${BASH_LINENO[$((i-1))]}" \
       "${FUNCNAME[$i]}"
   done
+  return "$result"
 }
 
 # Main driver of ./go script functionality.
