c1423d75f814ade0453043440153eb571f118186
Nikolaos Kakouros <nkak@kth.se>
2017-12-12 17:47:09 +0100

    Exports $_GO_STANDALONE, plus one more unbound var

--- go-core.bash
+++ go-core.bash
@@ -65,9 +65,10 @@ fi
 declare -r -x _GO_CORE_DIR="$PWD"
 
 # Set _GO_STANDALONE if your script is a standalone program.
-#
+declare -x _GO_STANDALONE="${_GO_STANDALONE-}"
+
 # See the "Standalone mode" section of README.md for more information.
-if [[ "${_GO_STANDALONE:-null}" == "null" ]]; then
+if [[ -n "$_GO_STANDALONE" ]]; then
   cd "$_GO_ROOTDIR" || exit 1
 else
   cd "$__go_orig_dir" || exit 1
